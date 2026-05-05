26afa39226aaf597025bcdaabdc705d009826928
eghafoo <c28eric.ghafoor@afacademy.af.edu>
2026-05-05 15:00:38 -0600

    Add files via upload

--- src/ALU.vhd
+++ src/ALU.vhd
@@ -57,40 +57,42 @@ alu : process(i_A, i_B, i_op)
     variable notB : unsigned(8 downto 0);
     variable v_result_9 : std_logic_vector(8 downto 0);
     variable v_result_8 : std_logic_vector(7 downto 0);
+    variable v_N, v_Z, v_C, v_V : std_logic;
     begin
         A := unsigned('0' & i_A);
         B := unsigned('0' & i_B);
         notB := unsigned(not('0' & i_B));
         
         case i_op is    
-            when "000" => result_9 <= std_logic_vector(A+B); --add
-            when "001" => result_9 <= std_logic_vector(A + notB + 1); --subtract
-            when "010" => result_9 <= '0' & (i_A and i_B); --and
-            when "011" => result_9 <= '0' & (i_A or i_B); --or
-            when others => result_9 <= '0' & i_A;
+            when "000" => v_result_9 := std_logic_vector(A+B); --add
+            when "001" => v_result_9 := std_logic_vector(A + notB + 1); --subtract
+            when "010" => v_result_9 := '0' & (i_A and i_B); --and
+            when "011" => v_result_9 := '0' & (i_A or i_B); --or
+            when others => v_result_9 := '0' & i_A;
         end case; 
         v_result_8 := v_result_9(7 downto 0);
 
-    flag_N <= v_result_8(7);
+    v_N := v_result_8(7);
 
     if v_result_8 = x"00" then
-        flag_Z <= '1';
+        v_Z := '1';
     else
-        flag_Z <= '0';
+        v_Z := '0';
     end if;
 
-    flag_C <= v_result_9(8);
+    v_C := v_result_9(8);
     case i_op is
-        when "000" => flag_V <= (not (i_A(7) xor i_B(7))) and (i_A(7) xor v_result_8(7));
-        when "001" => flag_V <= (i_A(7) xor i_B(7)) and (i_A(7) xor v_result_8(7));
-        when others => flag_V <= '0';
+        when "000" => v_V := (not (i_A(7) xor i_B(7))) and (i_A(7) xor v_result_8(7));
+        when "001" => v_V := (i_A(7) xor i_B(7)) and (i_A(7) xor v_result_8(7));
+        when others => v_V := '0';
     end case;
+    o_result <= v_result_8;
+    o_flags  <= v_N & v_Z & v_C & v_V;
     result_9 <= v_result_9;
     result_8 <= v_result_8;
+    flag_N   <= v_N;
+    flag_Z   <= v_Z;
+    flag_C   <= v_C;
+    flag_V   <= v_V;
 end process alu;
-result_8 <= result_9 (7 downto 0);
-
-o_result <= result_8;
-o_flags <= flag_N & flag_Z & flag_C & flag_V;
-
 end Behavioral;
